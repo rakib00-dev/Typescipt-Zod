22158199b7a47416a996b7958876914bb81d3b7c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-21 20:12:02 +0600

    learned about optional parse

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { z } from 'zod';
 
 const UserSchema = z.object({
   username: z.string(),
+  age: z.number().optional(),
+  dateOfBirth: z.date().optional(),
+  isProgramming: z.boolean().optional(),
 });
 
 type User = z.infer<typeof UserSchema>;
 
 const user: User = { username: 'asdf' };
-console.log(UserSchema.safeParse(user));
+console.log(UserSchema.safeParse(user).success);
