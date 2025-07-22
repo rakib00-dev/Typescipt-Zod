030e26e67c4d639297f15d901cec5e12fc86b3fe
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-22 23:08:00 +0600

    learned about the default schema and we can also pass function to it

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { z } from 'zod';
 
 const UserSchema = z.object({
   username: z.string().min(3).max(5),
-  age: z.number().gt(0),
+  age: z.number().default(Math.random()),
   dateOfBirth: z.date().optional(),
-  isProgramming: z.boolean().nullish(),
+  isProgramming: z.boolean().nullish().default(true),
 });
 
 type User = z.infer<typeof UserSchema>;
 
-const user: User = { username: 'dasdf', age: 1, isProgramming: undefined };
-console.log(UserSchema.safeParse(user).success);
+const user: User = { username: 'dasdf' };
+console.log(UserSchema.safeParse(user).data);
