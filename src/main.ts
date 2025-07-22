be88387ace4b827e063252e2742a270994df5328
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-22 23:04:46 +0600

    learned about nullish and nullable

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ const UserSchema = z.object({
   username: z.string().min(3).max(5),
   age: z.number().gt(0),
   dateOfBirth: z.date().optional(),
-  isProgramming: z.boolean().optional(),
+  isProgramming: z.boolean().nullish(),
 });
 
 type User = z.infer<typeof UserSchema>;
 
-const user: User = { username: 'dasdf', age: 1 };
+const user: User = { username: 'dasdf', age: 1, isProgramming: undefined };
 console.log(UserSchema.safeParse(user).success);
