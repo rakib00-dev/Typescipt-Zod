903ff94101147a6c80d84760bc6368d1d3746bb0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-22 22:56:57 +0600

    tryed the optional and min and max

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { z } from 'zod';
 
 const UserSchema = z.object({
-  username: z.string(),
+  username: z.string().min(3).max(5),
   age: z.number().optional(),
   dateOfBirth: z.date().optional(),
   isProgramming: z.boolean().optional(),
-  null: z.null().optional(),
 });
 
 type User = z.infer<typeof UserSchema>;
 
-const user: User = { username: 'asdf' };
+const user: User = { username: 'asdfasdfasdf' };
 console.log(UserSchema.safeParse(user).success);
