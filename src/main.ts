79a852f0e270e27be7133293c1650821e797c598
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-22 23:01:53 +0600

    used greter then option

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { z } from 'zod';
 
 const UserSchema = z.object({
   username: z.string().min(3).max(5),
-  age: z.number().optional(),
+  age: z.number().gt(0),
   dateOfBirth: z.date().optional(),
   isProgramming: z.boolean().optional(),
 });
 
 type User = z.infer<typeof UserSchema>;
 
-const user: User = { username: 'asdfasdfasdf' };
+const user: User = { username: 'dasdf', age: 1 };
 console.log(UserSchema.safeParse(user).success);
