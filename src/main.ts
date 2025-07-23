d439c12342270803de2c5feecc2fd9a3c116b0d5
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 09:50:49 +0600

    learned about tuple and how does this work

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ const Hobbies = ['Programming', 'Walking', 'Sleeping'] as const;
 
 const UserSchema = z.object({
   username: z.string().min(3).max(5),
-  friends: z
-    .array(z.string())
-    .default(['afonaf', 'kanon', 'shohag', 'rohan', 'shimul', '2akash']),
+  friends: z.array(z.string()).optional(),
+  coards: z.tuple([z.string(), z.number()]).rest(z.string()),
 });
 
 type User = z.infer<typeof UserSchema>;
 
 const user: User = {
   username: 'rakib',
+  coards: ['d', 13],
 };
 console.log(UserSchema.parse(user));
