7657eb7d3e6668b68550e7ae78e90550705a7340
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 21:44:09 +0600

    learned about fromZodError

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { z } from 'zod';
+import { fromZodError } from 'zod-validation-error';
 
-const brandEmail = z
-  .string()
-  .refine((val) => val.endsWith('@rakibdeveloper.com'), {
-    error: 'Email Must End @rakibdeveloper.com',
-  });
+const UserSchema = z.object({
+  username: z.string().min(3, 'must be add upto 3 char').max(5),
+  friends: z.array(z.string()).optional(),
+  coards: z.tuple([z.string(), z.number()]).rest(z.string()),
+});
 
-console.log(brandEmail.parse('geasdflo@rakibdeveloper.com'));
+type User = z.infer<typeof UserSchema>;
+
+const user: User = {
+  username: '12',
+  coards: ['d', 13],
+};
+const result = UserSchema.safeParse(user);
+
+if (!result.success) {
+  console.log(fromZodError(result.error));
+}
