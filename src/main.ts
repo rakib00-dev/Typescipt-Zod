56ff7848da389f898b0584c465c06e06532292a6
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 07:12:23 +0600

    learned about partial() deepPartial() and omit

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,20 @@ import { z } from 'zod';
 
 const Hobbies = ['Programming', 'Walking', 'Sleeping'] as const;
 
-const UserSchema = z.object({
-  username: z.string().min(3).max(5),
-  age: z.number().default(Math.random()).optional(),
-  dateOfBirth: z.date().optional(),
-  isProgramming: z.boolean().nullish().default(true),
-  hobby: z.enum(Hobbies),
-});
+const UserSchema = z
+  .object({
+    username: z.string().min(3).max(5),
+    age: z.number().default(Math.random()).optional(),
+    dateOfBirth: z.date().optional(),
+    isProgramming: z.boolean().nullish().default(true),
+    hobby: z.enum(Hobbies),
+  })
+  .omit({ username: true });
 
 type User = z.infer<typeof UserSchema>;
 
 const user: User = {
-  username: 'dasdf',
+  age: 2,
   dateOfBirth: new Date(),
   isProgramming: true,
   hobby: 'Programming',
