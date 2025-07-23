7feef66ac4dcdf69ea477ae189cfc932bdffe994
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 06:57:40 +0600

    learned about enum and how does this work and define enum outside of UserSchema

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { z } from 'zod';
 
+enum Hobbies {
+  Programming = 'coding',
+  Walking,
+  Sleeping,
+}
+
 const UserSchema = z.object({
   username: z.string().min(3).max(5),
-  age: z.number().default(Math.random()),
+  age: z.number().default(Math.random()).optional(),
   dateOfBirth: z.date().optional(),
   isProgramming: z.boolean().nullish().default(true),
-  hobby: z.enum(['Programming', 'Walking', 'Sleeping']),
+  hobby: z.enum(Hobbies),
 });
 
 type User = z.infer<typeof UserSchema>;
 
-const user: User = { username: 'dasdf', hobby: 'Programming' };
+const user: User = {
+  username: 'dasdf',
+  dateOfBirth: new Date(),
+  isProgramming: true,
+  hobby: Hobbies.Programming,
+};
 console.log(UserSchema.parse(user));
