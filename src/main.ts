5708df189fd875f582b6eb5c4261e72c932f204b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 20:43:00 +0600

    leared about set's in function

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { z } from 'zod';
 
 const useMap = z.map(z.string(), z.object({ name: z.string() }));
 
-const user = new Map([
-  ['id-23', { name: 'rakib' }],
-  ['id-33', { name: 'romayen' }],
-]);
-console.log(useMap.parse(user));
+const user = new Set([1, 1, 1, 2, 3]);
+console.log(user);
