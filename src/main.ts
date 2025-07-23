c32df783c6d0ab8a39d48a3dec1742ca7ffd6d2b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 07:39:31 +0600

    learned about array and noempty() function

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,16 @@ import { z } from 'zod';
 
 const Hobbies = ['Programming', 'Walking', 'Sleeping'] as const;
 
-const UserSchema = z
-  .object({
-    username: z.string().min(3).max(5),
-  })
-  .strict();
-// .loose()
+const UserSchema = z.object({
+  username: z.string().min(3).max(5),
+  friends: z
+    .array(z.string())
+    .default(['afonaf', 'kanon', 'shohag', 'rohan', 'shimul', '2akash']),
+});
 
 type User = z.infer<typeof UserSchema>;
 
 const user: User = {
   username: 'rakib',
-  name: 'Muhammad',
 };
 console.log(UserSchema.parse(user));
