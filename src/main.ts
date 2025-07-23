34cc56a16a0c4950fb303962b29cf733b6a5cd3d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-23 09:54:37 +0600

    learned about the number and nunion function and how to use it in id

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import { z } from 'zod';
 const Hobbies = ['Programming', 'Walking', 'Sleeping'] as const;
 
 const UserSchema = z.object({
-  username: z.string().min(3).max(5),
-  friends: z.array(z.string()).optional(),
-  coards: z.tuple([z.string(), z.number()]).rest(z.string()),
+  id: z.string().or(z.number()),
+  // id: z.union([z.string(), z.number()]),
 });
 
 type User = z.infer<typeof UserSchema>;
 
 const user: User = {
-  username: 'rakib',
-  coards: ['d', 13],
+  id: 1,
 };
 console.log(UserSchema.parse(user));
